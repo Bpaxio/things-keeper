2ea227d867729a4ae7be76506637f289a337de12
Anastasia <sugar@smartdec.ru>
2019-05-20 23:12:43 +0300

    added simple cors mapping

--- src/main/java/ru/bbpax/keeper/configurarion/WebConfig.java
+++ src/main/java/ru/bbpax/keeper/configurarion/WebConfig.java
@@ -7,9 +7,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -28,5 +30,15 @@ public class WebConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("Content-Type", "Accept", "Total-Count", "loginInfo")
+                .exposedHeaders("Content-Type", "Date", "Total-Count", "loginInfo")
+                .maxAge(3600);
     }
 }
