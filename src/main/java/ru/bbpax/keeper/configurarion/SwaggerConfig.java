40891bb4306ba5caa08e53dd6a490c816759b252
Bpaxio <>
2019-05-21 21:36:47 +0300

    updated swagger config

--- src/main/java/ru/bbpax/keeper/configurarion/SwaggerConfig.java
+++ src/main/java/ru/bbpax/keeper/configurarion/SwaggerConfig.java
@@ -1,9 +1,10 @@
 package ru.bbpax.keeper.configurarion;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,19 +22,26 @@ import java.util.List;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .pathMapping("/")
+                .forCodeGeneration(true)
                 .securityContexts(Collections.singletonList(securityContext()))
                 .securitySchemes(Collections
-                        .singletonList(new ApiKey("TOKEN", HttpHeaders.AUTHORIZATION, "header")))
+                        .singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", HttpHeaders.AUTHORIZATION, "header");
     }
 
     private SecurityContext securityContext() {
@@ -47,7 +55,7 @@ public class SwaggerConfig {
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = new AuthorizationScope("global", "accessEverything");
         return Collections.singletonList(
-                new SecurityReference("TOKEN", authorizationScopes));
+                new SecurityReference("JWT", authorizationScopes));
     }
 
     private ApiInfo apiInfo() {
--- src/main/java/ru/bbpax/keeper/configurarion/security/WebSecurityConfig.java
+++ src/main/java/ru/bbpax/keeper/configurarion/security/WebSecurityConfig.java
@@ -36,7 +36,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/");
+        web.ignoring()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+        );
     }
 
     @Override
