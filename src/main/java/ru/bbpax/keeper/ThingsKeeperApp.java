e72051cb42af15158e14cb32cb631806f86937a0
BPaxio <bbpax@yandex.ru>
2019-05-23 22:18:17 +0300

    added discovery client for eureka

--- src/main/java/ru/bbpax/keeper/ThingsKeeperApp.java
+++ src/main/java/ru/bbpax/keeper/ThingsKeeperApp.java
@@ -3,9 +3,11 @@ package ru.bbpax.keeper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableMongoRepositories
 public class ThingsKeeperApp {
